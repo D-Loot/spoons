23de059e122bf16ef99df70f44e70dfc7333dcba
Keely Bailey <keelybailey3@gmail.com>
2021-06-19 11:36:03 -0500

    Update boolean to string

--- Timer/scripts.js
+++ Timer/scripts.js
@@ -42,7 +42,7 @@ function startTimer() {
 
       for (let i = 1; i < 4; i++) {
         console.log(localStorage.getItem(`${dayValue}-task${i}Completed`));
-        if (localStorage.getItem(`${dayValue}-task${i}Completed`) === false) {
+        if (localStorage.getItem(`${dayValue}-task${i}Completed`) === "false") {
           x = 1;
         } else {
           console.log(localStorage.getItem(`${dayValue}-task${i}Completed`));
