6b17503162eaec10d68af985ae893196a6ad8fc7
Dan Looten <danwlooten@gmail.com>
2021-06-22 16:47:41 -0500

    Update API Javascript and CSS styling

--- api.js
+++ api.js
@@ -1,6 +1,28 @@
-const numbers = {
-  odd: 1,
-  even: 2,
-};
-
-export default numbers;
+// QUOTE API
+const quote = document.getElementById("quote");
+generateQuote();
+function generateQuote() {
+  fetch("https://api.kanye.rest")
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      quote.innerHTML = `
+      <h3 class="title is-3">"${data.quote}"</h3><h4 class="title is-4">-Kanye West</h4>`;
+    });
+}
+// KITTEN API
+const kittenContainer = document.getElementById("kittenContainer");
+generateCat();
+function generateCat() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data[0].url);
+      kittenContainer.innerHTML = `<img src="${data[0].url}" alt="cat picture">`;
+    });
+}
