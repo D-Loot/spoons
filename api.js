3e183f4dc0b2d416a111f6f33ac875fe574c4926
Keely Bailey <keelybailey3@gmail.com>
2021-06-22 14:10:59 -0500

    Add api code

--- api.js
+++ api.js
@@ -1,6 +1,57 @@
-const numbers = {
-  odd: 1,
-  even: 2,
-};
+// QUOTE API
 
-export default numbers;
+const quote = document.getElementById("quote");
+
+/*
+const ZQ_API_KEY = "0357db8750cf52337319cbfb3f540da9d1fead89";
+const ZQ_API_URL =
+  "https://zenquotes.io/api/today&key=0357db8750cf52337319cbfb3f540da9d1fead89";
+
+generateQuote();
+
+async function generateQuote(
+  url = "https://zenquotes.io/api/today&key=0357db8750cf52337319cbfb3f540da9d1fead89"
+) {
+  const response = await fetch(url, {
+    method: "GET",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
+  const data = await response.json();
+
+  console.log(data);
+}
+*/
+generateQuote();
+
+function generateQuote() {
+  fetch("https://api.kanye.rest")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      quote.innerHTML = `
+      <h3 class="title is-3">"${data.quote}"</h3>`;
+    });
+}
+
+// KITTEN API
+
+const kittenContainer = document.getElementById("kittenContainer");
+
+generateCat();
+
+function generateCat() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0].url);
+      kittenContainer.innerHTML = `<img src="${data[0].url}" alt="cat picture">`;
+    });
+}
